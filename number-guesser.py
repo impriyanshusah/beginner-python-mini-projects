21c83eb6ef25e65f680195a8b6205c098198a722
Priyanshu Kumar <impriyanshusah@gmail.com>
2025-03-28 16:33:16 +0530

    Commited number guesser game

---
+++ number-guesser.py
@@ -0,0 +1,51 @@
+import random
+
+print("Welcome to the Number Guesser Game!")
+
+playing = input("Do you want to play? (yes/no) : ")
+if playing.lower() != "yes":
+    quit()
+
+print("Great! Let's play!")
+print(
+    "\nYou need to select a range of numbers\nAnd then try to guess the random number\nThat the computer has chosen within 3 guesses.\n"
+)
+
+
+topRange = input("Enter the top of the range: ")
+
+if topRange.isdigit():
+    topRange = int(topRange)
+
+    if topRange <= 0:
+        print("Please enter a number greater than 0")
+        quit()
+
+else:
+    print("Please enter a number")
+    quit()
+
+randomNumber = random.randint(0, topRange)
+guesses = 0
+
+while guesses < 3:
+    guesses += 1
+    userGuess = input("Make a guess: ")
+    if userGuess.isdigit():
+        userGuess = int(userGuess)
+    else:
+        print("Please enter a number next time.")
+        continue
+
+    if userGuess == randomNumber:
+        print(f"Congratulations! You guessed the correct number in {guesses} guesses!")
+        break
+    elif userGuess > randomNumber:
+        print("Guess lower")
+    else:
+        print("Guess higher")
+
+if guesses >= 3:
+    print(f"Sorry! You did not guess the correct number. The number was {randomNumber}")
+
+print("Thanks for playing!")
